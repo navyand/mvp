9a772fc38eddf1b9a7db4237b590a9d1dd56c524
navyand <nverar@unal.edu.co>
2025-06-11 23:09:02 -0500

    Contar visitas

--- app/main.py
+++ app/main.py
@@ -1,45 +1,44 @@
 from flask import Flask
 import os
-import logging
-from google.cloud import monitoring_v3
-from google.cloud.monitoring_v3 import types
-from google.auth import default
 import time
+from google.cloud import monitoring_v3
+from google.cloud.monitoring_v3.types import Point, TimeInterval, TypedValue, TimeSeries
 
 app = Flask(__name__)
-
-# Configura el logger para que se vean los logs en Cloud Logging
-logging.basicConfig(level=logging.INFO)
-
-# Autenticación implícita (GCP lo maneja en Cloud Run)
-credentials, project_id = default()
 
 
 def enviar_metricas():
     client = monitoring_v3.MetricServiceClient()
+    project_id = os.environ["GOOGLE_CLOUD_PROJECT"]
+    project_name = f"projects/{project_id}"
 
-    series = monitoring_v3.TimeSeries()
-    series.metric.type = "custom.googleapis.com/salus_mvp/solicitudes"
+    series = TimeSeries()
+    series.metric.type = "custom.googleapis.com/salus_mvp/visitas"
     series.resource.type = "global"
-    series.resource.labels["project_id"] = project_id
-    series.metric.labels["endpoint"] = "/"
 
     now = time.time()
     seconds = int(now)
     nanos = int((now - seconds) * 10**9)
 
-    interval = monitoring_v3.TimeInterval(
+    interval = TimeInterval(
         {
             "end_time": {"seconds": seconds, "nanos": nanos},
+            "start_time": {
+                "seconds": seconds,
+                "nanos": nanos,
+            },  # Para DELTA está bien si es igual
         }
     )
 
-    point = monitoring_v3.TypedValue(double_value=1.0)
+    # Enviar +1 por cada visita
+    value = TypedValue(double_value=1.0)
+    point = Point({"interval": interval, "value": value})
+    series.points.append(point)
 
-    series.points.append(monitoring_v3.Point({"interval": interval, "value": point}))
+    # Imprimir log para confirmar
+    print("✅ Enviando métrica personalizada: +1 visita")
 
-    client.create_time_series(name=f"projects/{project_id}", time_series=[series])
-    logging.info("✅ Métrica personalizada enviada a Cloud Monitoring")
+    client.create_time_series(name=project_name, time_series=[series])
 
 
 @app.route("/")
